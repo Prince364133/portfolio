cbdb85c23000230c9e303e410dae2e50d2bd5bf5
Prince364133 <monsterprince3641@gmail.com>
2025-01-06 17:45:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -184,7 +184,7 @@ const projects = [
     {
         title: "Snap Shiksha",
         description: "teacher booking educational platform.",
-        image: "logo.jpg",
+        image: "logo.png",
         link: "https://673f764d7a24a.site123.me/"
     }
 ];
